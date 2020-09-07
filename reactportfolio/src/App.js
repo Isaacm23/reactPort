76a17e4258f5fef002c748246dc06f1fff3aa309
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-09-07 12:10:52 -0500

    added to app.js

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import {BrowserRouter as Router} from 'react-router-dom'
 import './App.css';
+import { Container, NavbarBrand } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Navbar'
+import Footer from './components/Footer';
+import PortfolioPage from './pages/PortfolioPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
  
   constructor(props){
+    super(props);
     this.state = {
       title: 'Isaac Mckittrick',
       headerLinks: [
@@ -26,13 +34,39 @@ class App extends React.Component {
       title:'contact',
       subTitle: 'Please leave contact information if interested',
       text: 'I hope to hear from you'
-  },
- 
-  render(){
-  return (
-    <div> Hello world </div>
-  );
+  }
 }
+  }
+ 
+  
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Isaac Mckittrick</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">About</Link>
+                <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <AboutPage title={this.state.home.title} subTitle={this.state.about.subTitle} text={this.state.home.text} />} />
+          <Route path="/portfolio" render={() => <PortfolioPage title={this.state.portfolio.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          
+          <Footer />
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
